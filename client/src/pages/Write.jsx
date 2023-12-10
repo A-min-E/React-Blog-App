ee7df06f47349b6411a12e822b60bfc32672add9
Amine <wissarmohamedamine@gmail.com>
2023-12-10 15:18:41 +0100

    building the write page

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,5 +1,14 @@
 const Write = () => {
-  return <div></div>;
+  return (
+    // 44:05
+    <div className="add">
+      <div className="content"></div>
+      <div className="menu">
+        <div className="item">i1</div>
+        <div className="item">i2</div>
+      </div>
+    </div>
+  );
 };
 
 export default Write;
