d7c964acbeb1da4582226ef89620e820c5905847
Amine <wissarmohamedamine@gmail.com>
2023-12-10 19:32:33 +0100

    :(

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,13 +1,51 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+// 1:16:21
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const handelChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/auth/register", inputs);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="auth">
       <h1>Register</h1>
       <form action="">
-        <input required type="text" placeholder="username" />
-        <input required type="email" placeholder="email" />
-        <input required type="text" placeholder="password" />
-        <button>Register</button>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handelChange}
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={handelChange}
+        />
+        <input
+          required
+          type="text"
+          placeholder="password"
+          name="password"
+          onChange={handelChange}
+        />
+        <button onClick={handelSubmit}>Register</button>
         <p>This is an error</p>
         <span>
           Do you have an account? <Link to="/login">Login</Link>
